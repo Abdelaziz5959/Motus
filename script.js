651e7ddf331f4483da60efea3d8dfeafde9ef71f
Oussama Bacha <oussa.bacha@hotmail.com>
2024-09-11 15:55:33 +0200

    modif js et html

--- script.js
+++ script.js
@@ -1,62 +1,36 @@
+//              Nos variables                  
 
-let deleteKey = document.querySelector(".delete");
-let enterKey = document.querySelector(".enter");
-let spaceKey = document.querySelector(".space");
-let capsLock = document.querySelector(".capslock");
 let allKey = document.querySelectorAll(".key");
-let isCaps = false;
+let supp = document.querySelector(".delete");
+let keyEnter = document.querySelector(".enter");
+let tableau = document.querySelector(".gridContainer");
 
-deleteKey.addEventListener("click",function(){
-    let textContainerContent = textContainer.innerText;
-    if(textContainerContent.length == 0){
+//       Fonction touche delete
+
+supp.addEventListener("click",function(){
+    let tableauContent = gridContainer.innerText;
+    if(tableauContent.length == 0){
         return;
     }
-    console.log(textContainerContent);
-    let newContent = textContainerContent.slice(0,textContainerContent.length-1);
-    textContainer.innerText = newContent;
-})
+    console.log(tableauContent);
+    let newContent = tableauContent.slice(0,tableauContent.length-1);
+    gridContainer.innerText = newContent;
+});
 
-enterKey.addEventListener("click",function(){
-    let content = textContainer.innerText;
-    let newContent = content+"\n";
-    textContainer.innerText = newContent;
-})
-
-spaceKey.addEventListener("click",function(){
-    let content = textContainer.innerText;
-    let newContent = content+ '\u00A0';
-    textContainer.innerText = newContent;
-})
-
-capsLock.addEventListener("click",function(){
-    if(isCaps){
-        capsLock.classList.remove("active");
-        for(let key of allKey){
-            if(key.classList.contains("delete") || key.classList.contains("enter")||
-             key.classList.contains("space") || key.classList.contains("capslock")){
-                //nothing
-            }else
-                key.innerText = key.innerText.toLowerCase();
-        }
-    }else{
-        capsLock.classList.add("active");
-        for(let key of allKey){
-                if(key.classList.contains("delete") || key.classList.contains("enter")||
-                 key.classList.contains("space") || key.classList.contains("capslock")){
-                    //nothing
-                }else
-                    key.innerText = key.innerText.toUpperCase();
-            }
-        }
-    isCaps=!isCaps
-})
+//          Boucle all key
 
 for(let key of allKey){
     key.addEventListener("click",function(){
-        if(key.classList.contains("delete") || key.classList.contains("enter")||
-         key.classList.contains("space") || key.classList.contains("capslock")){
-            return;
+        
+        if(key.classList.contains("key delete") || key.classList.contains("key enter")
+         ){
+            return;  
+            
         }
-        textContainer.innerText += key.innerText;
+       
+        console.log(key);
+        gridContainer.innerText += key.innerText;
     })
-}
+};
+
+
