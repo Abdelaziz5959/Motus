a85ee79a79d4c763a7b8d40ff129ad762fe3f2a2
Abdelaziz590 <159035240+Abdelaziz590@users.noreply.github.com>
2024-09-12 13:51:47 +0200

    correction js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // Sélection des touches du clavier
-
-let allKey = document.querySelectorAll(".key");
+let allKey = document.getElementsByClassName("key");
 let supp = document.querySelector(".delete");
 let keyEnter = document.querySelector(".enter");
-let tableau = document.querySelector("#gridContainer"); // Correction du sélecteur
+let tableau = document.getElementById("gridContainer"); // Correction du sélecteur
 let bibiliotheque = ["ABBAYE", "BABINE", "CADRAN", "DECLIN", "FARINE", "JOCKEY", "PAPIER", "TALENT", "VENTRE", "WEBCAM"];
 console.log(bibiliotheque);
 
 // Sélection des cellules de la grille
-let cells = document.querySelectorAll(".gridCell");
+let cells = document.getElementsByClassName("gridCell");
 let currentCellIndex = 0; // Garde la trace de la cellule actuelle
-addEventListener
+
 // Fonction pour la touche delete
 supp.addEventListener("click", function () {
     if (currentCellIndex > 0) {
@@ -35,39 +34,6 @@ for (let key of allKey) {
         if (currentCellIndex < cells.length) {
             cells[currentCellIndex].innerText = key.innerText; // Ajouter la lettre dans la cellule actuelle
             currentCellIndex++; // Passer à la cellule suivante
-=======
-let allKey = document.getElementsByClassName("key");
-let supp = document.querySelector(".delete");
-let keyEnter = document.querySelector(".enter");
-let tableau = document.getElementById("gridContainer"); 
-
-// cellule du tableau défini par 0
-let cells = document.getElementsByClassName("gridCell");
-let currentCellIndex = 0; 
-
-// touche supprimer
-supp.addEventListener("click", function () {
-    if (currentCellIndex > 0) {
-        currentCellIndex--; 
-        cells[currentCellIndex].innerText = ""; 
-    }
-});
-
-// touche entrer
-keyEnter.addEventListener("click", function () {
-     
-});
-
-// Ajouter une lettre dans le tableau
-for (let key of allKey) {
-    key.addEventListener("click", function () {
-        if (key.classList.contains("delete") || key.classList.contains("enter")) {
-            return; 
-        }
-
-        if (currentCellIndex < cells.length) {
-            cells[currentCellIndex].innerText = key.innerText; 
-            currentCellIndex++; 
         }
     });
 }
